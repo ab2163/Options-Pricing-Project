1ffe5bb7dbe3b16e090fed6e9b876a9a197f1977
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-01-02 19:38:07 +0000

    Rudimentary model trains without bugs

--- options-pricing-draft.py
+++ options-pricing-draft.py
@@ -2,50 +2,42 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from py_vollib.black_scholes import black_scholes
-#from py_vollib.black_scholes.implied_volatility import implied_volatility
-#from py_vollib.black_scholes.greeks import analytical
 from random import uniform
-from sklearn.preprocessing import StandardScaler
-
-print('Imports complete')
+import numpy as np
 
 #Option parameters
 num_samples = 2100
 test_ratio = 1/7
-spots = [uniform(10, 500) for p in range(0, num_samples)]
-strikes = [uniform(0.6*spots[p], 1.1*spots[p]) for p in range(0, num_samples)]
-mat_times = [uniform(0.01, 2) for p in range(0, num_samples)]
-vols = [uniform(0.1, 0.2) for p in range(0, num_samples)]
-int_rates = [uniform(0.05, 0.20) for p in range(0, num_samples)]
+spots = np.array([uniform(10, 500) for p in range(0, num_samples)])
+strikes = np.array([uniform(0.6*spots[p], 1.1*spots[p]) for p in range(0, num_samples)])
+mat_times = np.array([uniform(0.01, 2) for p in range(0, num_samples)])
+vols = np.array([uniform(0.1, 0.2) for p in range(0, num_samples)])
+int_rates = np.array([uniform(0.05, 0.20) for p in range(0, num_samples)])
 
 #Black-Scholes option prices
 call_prices = []
 for i in range(0, num_samples):
 	call_prices.append(black_scholes('c', spots[i], strikes[i], mat_times[i], int_rates[i], vols[i]))
 
-print('Data generation complete')
-
 #Normalize data
-scaler = StandardScaler()
-spots = scaler.fit_transform(spots)
-strikes = scaler.fit_transform(srtikes)
-mat_times = scaler.fit_transform(mat_times)
-vols = scaler.fit_transform(vols)
-int_rates = scaler.fit_transform(int_rates)
-
-print('Data normalisation complete')
+spots = (spots - spots.mean())/spots.std()
+strikes = (strikes - strikes.mean())/strikes.std()
+mat_times = (mat_times - mat_times.mean())/mat_times.std()
+vols = (vols - vols.mean())/vols.std()
+int_rates = (int_rates - int_rates.mean())/int_rates.std()
 
 class OptionsDataset(Dataset):
     def __init__(self, training_set):
         if training_set:
-            self.len_data = int((1 - test_rato)*num_samples)
+            self.len_data = int((1 - test_ratio)*num_samples)
             self.offset = 0
         else:
-            self.len_data = int(test_rato*num_samples)
-            self.offset = int((1 - test_rato)*num_samples)
+            self.len_data = int(test_ratio*num_samples)
+            self.offset = int((1 - test_ratio)*num_samples)
 
     def __len__(self):
         return self.len_data
@@ -53,7 +45,7 @@ class OptionsDataset(Dataset):
     def __getitem__(self, i):
         i = i + self.offset
         input_vals = torch.tensor([spots[i], strikes[i], mat_times[i], int_rates[i], vols[i]])
-        label = torch.tensor(call_prices[i])
+        label = torch.tensor([call_prices[i]])
         return input_vals, label
 
 #Neural network definition
@@ -105,22 +97,23 @@ def test_loop(dataloader, model, loss_fn):
         for X, y in dataloader:
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            correct += sum([1 if (0.99*yval < mval < 1.01*yval) else 0 for (yval, mval) in zip(y, pred)])
 
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \nAccuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 #Main script
-training_data = OptionsDataset(true)
-test_data = OptionsDataset(false)
-train_dataloader = DataLoader(training_data, batch_size=64)
-test_dataloader = DataLoader(test_data, batch_size=64)
 model = NeuralNetwork()
+model.double()
 learning_rate = 1e-3
 batch_size = 64
-epochs = 1
-loss_fn = nn.CrossEntropyLoss()
+epochs = 10
+training_data = OptionsDataset(True)
+test_data = OptionsDataset(False)
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+loss_fn = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 for t in range(epochs):
